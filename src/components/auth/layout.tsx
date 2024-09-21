fc750462e27eee879562268d9c0c3dc0695dbe19
I Wayan Indra Sakti Sanjaya <bonjour.wayan@gmail.com>
2024-09-21 22:00:15 +0700

    chore: set up auth layout

--- src/components/auth/layout.tsx
+++ src/components/auth/layout.tsx
@@ -5,7 +5,6 @@ import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 
 import { paths } from '@/paths';
-import { DynamicLogo } from '@/components/core/logo';
 
 export interface LayoutProps {
   children: React.ReactNode;
@@ -21,20 +20,10 @@ export function Layout({ children }: LayoutProps): React.JSX.Element {
         minHeight: '100%',
       }}
     >
-      <Box sx={{ display: 'flex', flex: '1 1 auto', flexDirection: 'column' }}>
-        <Box sx={{ p: 3 }}>
-          <Box component={RouterLink} href={paths.home} sx={{ display: 'inline-block', fontSize: 0 }}>
-            <DynamicLogo colorDark="light" colorLight="dark" height={32} width={122} />
-          </Box>
-        </Box>
-        <Box sx={{ alignItems: 'center', display: 'flex', flex: '1 1 auto', justifyContent: 'center', p: 3 }}>
-          <Box sx={{ maxWidth: '450px', width: '100%' }}>{children}</Box>
-        </Box>
-      </Box>
       <Box
         sx={{
           alignItems: 'center',
-          background: 'radial-gradient(50% 50% at 50% 50%, #122647 0%, #090E23 100%)',
+          background: 'radial-gradient(50% 50% at 50% 50%, #3FA9F5 0%, #B7CFFF 100%)',
           color: 'var(--mui-palette-common-white)',
           display: { xs: 'none', lg: 'flex' },
           justifyContent: 'center',
@@ -43,25 +32,43 @@ export function Layout({ children }: LayoutProps): React.JSX.Element {
       >
         <Stack spacing={3}>
           <Stack spacing={1}>
-            <Typography color="inherit" sx={{ fontSize: '24px', lineHeight: '32px', textAlign: 'center' }} variant="h1">
-              Welcome to{' '}
-              <Box component="span" sx={{ color: '#15b79e' }}>
-                Devias Kit
+            <Typography color="black" sx={{ fontSize: '24px', lineHeight: '32px', textAlign: 'center' }} variant="h1">
+              Selamat Datang di{' '}
+              <Box component="span" sx={{ color: '#ED1C24' }}>
+                Big
+              </Box>
+              <Box component="span" sx={{ color: '#F15A24' }}>
+                Box
               </Box>
             </Typography>
-            <Typography align="center" variant="subtitle1">
-              A professional template that comes with ready-to-use MUI components.
+            <Typography color="black" align="center" variant="subtitle1">
+              Inovasi AI untuk Keputusan Bisnis Akurat Berbasis Data
             </Typography>
           </Stack>
-          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
             <Box
               component="img"
-              alt="Widgets"
-              src="/assets/auth-widgets.png"
-              sx={{ height: 'auto', width: '100%', maxWidth: '600px' }}
+              alt="BigBox Logo"
+              src="/assets/bigbox-logo.svg"
+              sx={{ height: 'auto', width: '100%', maxWidth: '300px' }}
             />
           </Box>
         </Stack>
+      </Box>
+      <Box sx={{ display: 'flex', flex: '1 1 auto', flexDirection: 'column' }}>
+        <Box sx={{ p: 3 }}>
+          <Box component={RouterLink} href={paths.home} sx={{ display: 'inline-block', fontSize: 0 }}>
+            <Box
+              component="img"
+              alt="BigBox Logo"
+              src="/assets/bigbox-long-logo.svg"
+              sx={{ height: '32px', width: '122px' }}
+            />
+          </Box>
+        </Box>
+        <Box sx={{ alignItems: 'center', display: 'flex', flex: '1 1 auto', justifyContent: 'center', p: 3 }}>
+          <Box sx={{ maxWidth: '450px', width: '100%' }}>{children}</Box>
+        </Box>
       </Box>
     </Box>
   );
