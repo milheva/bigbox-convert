723e7ea85c104ae58ef86041968aed2c5884ff8e
Frandi <frandicollage21@gmail.com>
2024-09-22 13:28:41 +0800

    refactor logo and user profile

--- src/components/dashboard/layout/main-nav.tsx
+++ src/components/dashboard/layout/main-nav.tsx
@@ -70,7 +70,7 @@ export function MainNav(): React.JSX.Element {
             <Avatar
               onClick={userPopover.handleOpen}
               ref={userPopover.anchorRef}
-              src="/assets/avatar.png"
+              src="/assets/bigbox-logo.svg"
               sx={{ cursor: 'pointer' }}
             />
           </Stack>
--- src/components/dashboard/layout/user-popover.tsx
+++ src/components/dashboard/layout/user-popover.tsx
@@ -57,9 +57,9 @@ export function UserPopover({ anchorEl, onClose, open }: UserPopoverProps): Reac
       slotProps={{ paper: { sx: { width: '240px' } } }}
     >
       <Box sx={{ p: '16px 20px ' }}>
-        <Typography variant="subtitle1">Sofia Rivers</Typography>
+        <Typography variant="subtitle1">Digistar Kece</Typography>
         <Typography color="text.secondary" variant="body2">
-          sofia.rivers@devias.io
+          kel48@digistar.id
         </Typography>
       </Box>
       <Divider />
